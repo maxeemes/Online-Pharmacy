2c38c4eb476f9193a080679ba02334a34715d2b5
max <max.s.99@yandex.ru>
2019-03-12 12:28:25 +0300

    Исправил отображение пользователей

--- assets/usersContent.php
+++ assets/usersContent.php
@@ -7,28 +7,24 @@ session_start();
   	$db = "pharmacy_db";
 	
 
-  	$conn = mysqli_connect($host, $login, $password, $db);
+  	$conn = mysqli_connect($host, $login, $passwd, $db);
 
   	if (!$conn) {
   		die("Connection failed: " . mysqli_connect_error());
-      exit;
   	}
 	
-		function myhash($passwd, $SALT) { 
-	
-        $hash = sha1($SALT . $passwd);  
-			
-        return $hash;  
-    }
-	
-	if(isset($_POST['addUser'])){
+		function myhash($passwd, $SALT)
+		{
+			$hash = sha1($SALT . $passwd);
+			return $hash;
+		}
+  	if(isset($_POST['addUser'])){
 		echo "<script>location.replace('register.php');</script>";		
 	}
 /////////////////////
+	if (isset($_GET['del_id'])) {
 
-				   if (isset($_GET['del_id'])) {
-
-					   	echo <<<HTML
+		echo <<<HTML
 					<br>
 							<div class="alert alert-danger" role="alert">
 								Вы действительно хотите удалить запись пользователя '{$_GET['del_id']}'?
@@ -40,46 +36,36 @@ session_start();
 						border-color:#010508;" class="btn btn-outline-secondary">
 							</form>
 HTML;
-					if (isset($_POST['delYesBTN'])) {
-						$del = $_GET['del_id'];
-						$sql = "DELETE FROM login WHERE login = '{$del}'";
-						$result = mysqli_query($conn, $sql);
-						echo <<<HTML
+		if (isset($_POST['delYesBTN'])) {
+			$del = $_GET['del_id'];
+			$sql = "DELETE FROM login WHERE login = '{$del}'";
+			$result = mysqli_query($conn, $sql);
+			echo <<<HTML
 		<br>
 		<div class="alert alert-success" role="alert">
 			Запись успешно удалена!
 		</div>	
 HTML;
-						sleep(1);
-						echo "<script>location.replace('users.php');</script>";
-					}
-					if (isset($_POST['delNoBTN'])) {
-						echo "<script>location.replace('users.php');</script>";
-					}
-				   }	
-	
-	
-	
-	
-	
-	
-	
-	
+			sleep(1);
+			echo "<script>location.replace('users.php');</script>";
+		}
+		if (isset($_POST['delNoBTN'])) {
+			echo "<script>location.replace('users.php');</script>";
+		}
+	}
 	///////////////
 $check = "SELECT * FROM login;";
               if (mysqli_query($conn, $check)) {
-                $show = "SELECT login,password, privilege FROM login ORDER BY privilege DESC";
-                $result =mysqli_query($conn, $show);
-                if ($result)
-                {
+				  $show = "SELECT login, password, status FROM login ORDER BY status DESC";
+				  $result = mysqli_query($conn, $show);
+				  if ($result) {
 
-                  echo <<<HEREDOC
+					  echo <<<HEREDOC
 						<form method='post'>
-						<input type="submit"  name="addUser" value="Добавить пользователя" style="width: 100%;color: black; 
+						<input type="submit"  name="addUser" value="Добавить пользователя" class="btn btn-primary btn-block">
 						</form>
-						border-color:#010508;height: 3rem;" class="btn btn-outline-secondary">
-                                <table class="table">
-                                <thead class="thead-light">
+                                <table class="table table-hover">
+                                <thead class="thead-dark">
                                     <tr>
                                         <th>Логин</td>
                                         <th>Пароль</td>
@@ -88,32 +74,31 @@ $check = "SELECT * FROM login;";
                                     </tr>
                                     </thead>
 HEREDOC;
-					
 
-                 while($row = mysqli_fetch_array($result)){
-					 $type;
-					 if($row['privilege'] == 0){
-						 $type="Пользователь";
-					 }else if($row['privilege'] == 1){
-						 $type="Админ";
-					 }else if($row['privilege'] == 2){
-						$type="Полные права"; 
-					 }
 
-					define("SALT_LENGHT", 10);
-					$r_min = 0;
-					$r_max = 21;
+					  while ($row = mysqli_fetch_array($result)) {
+						  $type;
+						  if ($row['status'] == 0) {
+							  $type = "Пользователь";
+						  } else if ($row['status'] == 1) {
+							  $type = "Админ";
+						  } else if ($row['status'] == 2) {
+							  $type = "Полные права";
+						  }
 
-					
-					$SALT = substr(md5(uniqid() . time), mt_rand($r_min, $r_max), SALT_LENGHT);
-					
-					$log = $_POST['log'];
-					$pas = $_POST['password'];
-					$hash = myhash($pas,$SALT);
-					 
-					 
-					 
-                    echo <<<HEREDOC
+						  define("SALT_LENGHT", 10);
+						  $r_min = 0;
+						  $r_max = 21;
+
+
+						  $SALT = substr(md5(uniqid() . time), mt_rand($r_min, $r_max), SALT_LENGHT);
+
+						  $log = $_POST['log'];
+						  $pas = $_POST['password'];
+						  $hash = myhash($pas, $SALT);
+
+
+						  echo <<<HEREDOC
                     <tr>
                     <td>{$row['login']}</td>
                     <td>{$row['password']}</td>
@@ -121,15 +106,15 @@ HEREDOC;
 					<td><a name=\"del_id\" href=\users.php?del_id={$row["login"]}>Удалить</a></td>
                     </tr>
 HEREDOC;
-				$ModifyName = '';
-                  }
-				 
-                }
-				
-              }
+						  $ModifyName = '';
+					  }
+
+				  }
+
+			  }
               else {
-                  echo "<script>alert('Ошибка Базы данных! Проверте наличие таблицы customer')</script>";
-              }
+				  echo "<script>alert('Ошибка Базы данных! Проверте наличие таблицы customer')</script>";
+			  }
 			  
 
-?>				
+?>
--- users.php
+++ users.php
@@ -20,25 +20,27 @@ if(isset($_POST['RecordDelBTN'])){
 <head>
 <title>Аптека</title>
 <meta charset=utf-8>
-  <link rel="stylesheet"
-  href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-  integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
-  crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
-<body style="background-color:#e3e8e8;">
+<body>
+<?php require_once("assets/header.php") ?>
 	<div class="container">
-			<?php require_once("assets/header.php") ?>
 		<div class="Main">
 			<div class="Nav">
 				<div class="Official">
-					<a href="index.php" style="padding-left: 1em;">Главная</a>
-				</div>
-				<div style="margin-left: 66%">
-				<?php require_once("assets/AddProdBTN.php") ?>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item" aria-current="page"><a href="index.php">Главная</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Пользователи</li>
+                        </ol>
+                    </nav>
 				</div>
 			</div>
 			<div class="Content">
-			<?php require_once("assets/usersContent.php") ?>
+			    <?php require_once("assets/usersContent.php") ?>
 			</div>
 		</div>
 	</div>
